10fabfccf634fe86230d133cfb0abf520d448b06
ruslan <bumerboy86@gmail.com>
2023-04-27 13:09:10 +0600

    #3

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,8 @@
-import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
-import './index.css';
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-	<App />
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+    // eslint-disable-next-line react/react-in-jsx-scope
+    <App />
 );
