c17a19b7e9b0683c4625a331b3f89815fd89b33a
albina_rakhmetova <rakhmetovaazh@gmail.com>
2023-05-05 12:34:23 +0600

    [ESDP-32] corrected request login

--- src/containers/RegisterUser/RegisterUser.tsx
+++ src/containers/RegisterUser/RegisterUser.tsx
@@ -11,7 +11,7 @@ const RegisterUser: React.FunctionComponent = (): React.ReactElement => {
         phone: "",
         patronim: "",
         email: "",
-        role: ERoles
+        role: ERoles.DOCTOR
     });
     
     const [createUser] = useCreateUserMutation();
