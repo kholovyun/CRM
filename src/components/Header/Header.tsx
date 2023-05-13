1152ba27cc1cfc70488e2f287354d25b5193d63d
izida kubekova <isedak81@gmail.com>
2023-05-13 07:22:04 +0600

    DP-18 Отредактирован компонент Header
    удалён компонент Navbar

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,18 +1,42 @@
 import { FunctionComponent, ReactElement } from "react";
-import { Container } from "../UI/Container/Container";
+import { useAppSelector } from "../../app/hooks";
+import { NavLink } from "react-router-dom";
 import styles from "./Header.module.css";
-import Navbar from "../Navbar/Navbar";
 import Logo from "../Logo/Logo";
+import { toast } from "react-toastify";
+import Btn from "../UI/Btn/Btn";
+import { EBtnSize } from "../../enums/EBtnSize";
+import { EBtnClass } from "../../enums/EBtnClass";
 
 const Header: FunctionComponent = (): ReactElement => {
+    const { user } = useAppSelector(state => state.auth);
+
     return (
-        <header>
-            <Container>
-                <div className={styles.Header_container}>
+        <header className={styles.header_bg_container}>
+            <div className={styles.header_content_container}>
+
+                <div className={styles.header_logo_box}>
                     <Logo />
-                    <Navbar />
-                </div>
-            </Container>
+                </div>                    
+                <nav className={styles.header_links_row}>
+                    <div className={styles.flex_row}>
+                        {user && 
+                            <NavLink to={"cabinet/"} className={styles.header_link}>
+                            Личный кабинет: <strong>{user.name}</strong>
+                            </NavLink>
+                        }
+                        <div className={styles.header_avatar_box}>
+                            <div className={styles.header_avatar_img}></div>
+                        </div>
+                        <Btn 
+                            title="Поддержка" 
+                            onclick={() => toast.info("Функционал пока недоступен")}
+                            size={EBtnSize.small} 
+                            btnClass={EBtnClass.dark_active}
+                        />
+                    </div>
+                </nav>
+            </div>
         </header>
     );
 };
