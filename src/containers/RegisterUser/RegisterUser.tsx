33cc0dbbca6085998ac6843ac214bea6da6b7f6e
izida kubekova <isedak81@gmail.com>
2023-05-16 09:12:24 +0600

    [DP-35] Отред-ны в связи с изменениями названий
    ForgotPassword, Login, ResetPassword, RegisterUser

--- src/containers/RegisterUser/RegisterUser.tsx
+++ src/containers/RegisterUser/RegisterUser.tsx
@@ -13,8 +13,9 @@ import { FetchBaseQueryError } from "@reduxjs/toolkit/dist/query";
 import { SerializedError } from "@reduxjs/toolkit";
 import { IErrorResponse } from "../../interfaces/IUser/IErrorResponse";
 import { IMessage } from "../../interfaces/IUser/IMessage";
-import { Contetnt } from "../../components/UI/Contetnt/Contetnt";
+import { FormBox } from "../../components/UI/FormBox/FormBox";
 import { Title } from "../../components/UI/Title/Title";
+import { Container } from "../../components/UI/Container/Container";
 
 
 const RegisterUser: React.FunctionComponent = (): React.ReactElement => {
@@ -47,62 +48,64 @@ const RegisterUser: React.FunctionComponent = (): React.ReactElement => {
     isSuccess && toast.info("Письмо активации отправлено на почту");
 
     return (
-        <Contetnt>
-            <Title text="Регистрация" />
-            <Formik
-                initialValues={{
-                    name: "",
-                    surname: "",
-                    patronim: "",
-                    phone: "",
-                    email: "",
-                    role: ""
-                }}
-                validateOnBlur
-                onSubmit={(values) => {
-                    createUser(values);
-                }}
-                validationSchema={validationSchemaRegUser}
-            >
-                {({ isValid, errors, touched, handleSubmit, handleChange, handleBlur }) => (
-                    <Form className={styles.LoginForm}>
-                        <Field className={styles.LoginInput} name="name" type="text" placeholder="Имя" />
-                        {touched.name && errors.name ? <p className={styles.typeError}>{errors.name}</p> : <p className={styles.typeText}></p>}
-                        <Field className={styles.LoginInput} name="surname" type="text" placeholder="Фамилия" />
-                        {touched.surname && errors.surname ? <p className={styles.typeError}>{errors.surname}</p> : <p className={styles.typeText}></p>}
-                        <Field className={styles.LoginInput} name="patronim" type="text" placeholder="Отчество" />
-                        {touched.patronim && errors.patronim ? <p className={styles.typeError}>{errors.patronim}</p> : <p className={styles.typeText}></p>}
-                        <Field
-                            name="phone"
-                            type="text"
-                            render={({ ...field }) => (
-                                <MaskedInput
-                                    {...field}
-                                    mask={phoneNumberMask}
-                                    id="phone"
-                                    placeholder="+7(___)___-__-__"
-                                    type="text"
-                                    onChange={handleChange}
-                                    onBlur={handleBlur}
-                                    className={styles.LoginInput}
-                                />
-                            )}
-                        >
-                        </Field>
-                        {touched.phone && errors.phone ? <p className={styles.typeError}>{errors.phone}</p> : <p className={styles.typeText}></p>}
-                        <Field className={styles.LoginInput} name="email" type="text" placeholder="Email" />
-                        {touched.email && errors.email ? <p className={styles.typeError}>{errors.email}</p> : <p className={styles.typeText}></p>}
-                        <Field as="select" name="role" className={styles.LoginInput}>
-                            <option value="">Выберите роль</option>
-                            <option value={ERoles.DOCTOR}>Врач</option>
-                            <option value={ERoles.ADMIN}>Админ</option>
-                        </Field>
-                        {touched.role && errors.role ? <p className={styles.typeError}>{errors.role}</p> : <p className={styles.typeText}></p>}
-                        <Btn disabled={!isValid} title="Создать" onclick={handleSubmit} size={EBtnSize.big} types={EBtnTypes.submit} />
-                    </Form>
-                )}
-            </Formik>
-        </Contetnt>
+        <Container>
+            <FormBox>
+                <Title text="Регистрация" />
+                <Formik
+                    initialValues={{
+                        name: "",
+                        surname: "",
+                        patronim: "",
+                        phone: "",
+                        email: "",
+                        role: ""
+                    }}
+                    validateOnBlur
+                    onSubmit={(values) => {
+                        createUser(values);
+                    }}
+                    validationSchema={validationSchemaRegUser}
+                >
+                    {({ isValid, errors, touched, handleSubmit, handleChange, handleBlur }) => (
+                        <Form className={styles.LoginForm}>
+                            <Field className={styles.LoginInput} name="name" type="text" placeholder="Имя" />
+                            {touched.name && errors.name ? <p className={styles.typeError}>{errors.name}</p> : <p className={styles.typeText}></p>}
+                            <Field className={styles.LoginInput} name="surname" type="text" placeholder="Фамилия" />
+                            {touched.surname && errors.surname ? <p className={styles.typeError}>{errors.surname}</p> : <p className={styles.typeText}></p>}
+                            <Field className={styles.LoginInput} name="patronim" type="text" placeholder="Отчество" />
+                            {touched.patronim && errors.patronim ? <p className={styles.typeError}>{errors.patronim}</p> : <p className={styles.typeText}></p>}
+                            <Field
+                                name="phone"
+                                type="text"
+                                render={({ ...field }) => (
+                                    <MaskedInput
+                                        {...field}
+                                        mask={phoneNumberMask}
+                                        id="phone"
+                                        placeholder="+7(___)___-__-__"
+                                        type="text"
+                                        onChange={handleChange}
+                                        onBlur={handleBlur}
+                                        className={styles.LoginInput}
+                                    />
+                                )}
+                            >
+                            </Field>
+                            {touched.phone && errors.phone ? <p className={styles.typeError}>{errors.phone}</p> : <p className={styles.typeText}></p>}
+                            <Field className={styles.LoginInput} name="email" type="text" placeholder="Email" />
+                            {touched.email && errors.email ? <p className={styles.typeError}>{errors.email}</p> : <p className={styles.typeText}></p>}
+                            <Field as="select" name="role" className={styles.LoginInput}>
+                                <option value="">Выберите роль</option>
+                                <option value={ERoles.DOCTOR}>Врач</option>
+                                <option value={ERoles.ADMIN}>Админ</option>
+                            </Field>
+                            {touched.role && errors.role ? <p className={styles.typeError}>{errors.role}</p> : <p className={styles.typeText}></p>}
+                            <Btn disabled={!isValid} title="Создать" onclick={handleSubmit} size={EBtnSize.big} types={EBtnTypes.submit} />
+                        </Form>
+                    )}
+                </Formik>
+            </FormBox>
+        </Container>
     );
 };
 
