9cc128c4d42e2cca3324858e2703cb9fb1d630b4
izida kubekova <isedak81@gmail.com>
2023-05-16 09:07:25 +0600

    [DP-35] Отред-н и переименован Contetnt в FormBox

---
+++ src/components/UI/FormBox/FormBox.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./FormBox.module.css";
+
+export const FormBox = ({ children }: { children: React.ReactNode }) => {
+    return (
+        <div className={styles.contentBox}>{children}</div>
+    );
+};
