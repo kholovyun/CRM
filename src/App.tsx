c717f5c0beb8bfbd38fe3f35bed8f14c4ad80515
Сатыбалдинов Ануар <paramode88@gmail.com>
2023-05-18 14:15:56 +0600

    DP-25_FIX

--- src/App.tsx
+++ src/App.tsx
@@ -10,10 +10,7 @@ import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { DoctorCabinet } from "./containers/DoctorCabinet/DoctorCabinet";
 import { Home } from "./containers/Home/Home";
-<<<<<<< src/App.tsx
-import RegisterParrent from "./containers/RegisterParrent/RegisterParrent";
-import DoctorCabinetPage from "./containers/DoctorCabinetPage/DoctorCabinetPage";
-=======
+
 import AdminPage from "./containers/AdminPage/AdminPage";
 import { ERoles } from "./enums/ERoles";
 import RegisterParent from "./containers/RegisterParrent/RegisterParent";
@@ -21,7 +18,8 @@ import PrivateRoute from "./permissionRoutes/PrivateRoute/PrivateRoute";
 import AllDoctors from "./containers/AdminPage/AdminTables/AllDoctors/AllDoctors";
 import RegisterDoctor from "./containers/RegisterUser/RegisterDoctor";
 import RegisterAdmin from "./containers/RegisterUser/RegisterAdmin";
->>>>>>> src/App.tsx
+import DoctorCabinetPage from "./containers/DoctorCabinetPage/DoctorCabinetPage";
+
 
 const App: React.FunctionComponent = (): React.ReactElement => {
     return (
@@ -44,8 +42,8 @@ const App: React.FunctionComponent = (): React.ReactElement => {
                     <Route path="/admin" element={<AdminPanel />} />
                     <Route path="/reset-password" element={<ResetPassword />} />
                     <Route path="/forgot-password" element={<ForgotPassword />} />
-                    <Route path="/cabinet" element={<DoctorCabinet />} />
-                    <Route path="/dr-cabinet" element={<DoctorCabinetPage />} />
+                    {/* <Route path="/cabinet" element={<DoctorCabinet />} /> */}
+                    <Route path="/cabinet" element={<DoctorCabinetPage />} />
                 </Route>
             </Routes>
         </BrowserRouter>
