02f0856229107ba5f76c86fa6483642a9c022bd7
izida kubekova <isedak81@gmail.com>
2023-05-22 18:39:37 +0600

    [DP-33] Добавлен UI/ Loader

---
+++ src/components/UI/Loader/Loader.tsx
@@ -0,0 +1,9 @@
+import "./Loader.css";
+
+const Loader: React.FunctionComponent = (): React.ReactElement => {
+    return (
+        <div className={"loader"}>Идёт загрузка...</div>
+    );
+};
+
+export default Loader;
