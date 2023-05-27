2b19c6716d730a766531b15326adebf80ea47c21
ruslan <bumerboy86@gmail.com>
2023-05-27 12:40:08 +0600

    [DP-70] Исправлена валидация номера в схеме так же исправлена маска для киргизского номера

--- src/schemas/validationSchemaRegUser.ts
+++ src/schemas/validationSchemaRegUser.ts
@@ -26,7 +26,7 @@ export const validationSchemaRegUser = yup.object().shape({
             return true;
         }),
     phone: yup.string()
-        .matches(/^\+7\(\d{3}\)\d{3}-\d{2}-\d{2}$/, "Не соответствует формату")
+        .matches(/^\+\d{1,3}\(\d{3}\)\d{3}-\d{2}-\d{2}$/, "Не соответствует формату")
         .required("Введите номер телефона"),
     email: yup.string()
         .email("Введите корректный Email")
