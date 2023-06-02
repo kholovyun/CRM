1edd349c9146370c7d875d89849595d88c1aecaf
izida kubekova <isedak81@gmail.com>
2023-06-02 17:17:39 +0600

    [DP-87] Удалены props для AllAdmins и AllDoctors
    и вместо надписи "No data" при отсутствии списка поставила "Нет данных"

--- src/containers/AdminPage/AdminTables/AllDoctors/AllDoctors.tsx
+++ src/containers/AdminPage/AdminTables/AllDoctors/AllDoctors.tsx
@@ -1,7 +1,11 @@
 import React, { useState } from "react";
 // import Pagination from "../../../../components/UI/Pagination/Pagination";
 import styles from "../AllTables.module.css";
-import { useActivateDoctorMutation, useBlockDoctorMutation, useGetDoctorsQuery, } from "../../../../app/services/doctors";
+import {
+    useActivateDoctorMutation,
+    useBlockDoctorMutation,
+    useGetDoctorsQuery,
+} from "../../../../app/services/doctors";
 import { FetchBaseQueryError } from "@reduxjs/toolkit/dist/query";
 import { SerializedError } from "@reduxjs/toolkit";
 import Modal from "../../../../components/UI/Modal/Modal";
@@ -20,14 +24,21 @@ import Loader from "../../../../components/UI/Loader/Loader";
 const AllDoctors: React.FunctionComponent = (): React.ReactElement => {
     const navigate = useNavigate();
     // const [currentPage, setCurrentPage] = useState(1);
-    const {data: doctors, error: getDoctorsError, isError: isDoctorsGetError, isLoading} = useGetDoctorsQuery({offset: 0, limit: 10});
-    const [blockThisUser, { error: blockUserError, isError: isBlockError }] = useBlockDoctorMutation();
-    const [activateThisDoctor, { error: activateError, isError: isActivateError }] = useActivateDoctorMutation();
+    const {
+        data: doctors,
+        error: getDoctorsError,
+        isError: isDoctorsGetError,
+        isLoading,
+    } = useGetDoctorsQuery({ offset: 0, limit: 10 });
+    const [blockThisUser, { error: blockUserError, isError: isBlockError }] =
+        useBlockDoctorMutation();
+    const [activateThisDoctor, { error: activateError, isError: isActivateError }] =
+        useActivateDoctorMutation();
 
     const [stateDoctor, setDoctor] = useState<IDoctorWithUser | null>(null);
     const [modalTitle, setModalTitle] = useState("");
     const [showModal, setShowModal] = useState(false);
-    
+
     const errorHandler = (data: FetchBaseQueryError | SerializedError | undefined) => {
         const err = data as IErrorResponse<IMessage>;
         toast.error(`Ошибка ${err.data.message} Статус: ${err.status}`);
@@ -42,29 +53,37 @@ const AllDoctors: React.FunctionComponent = (): React.ReactElement => {
         setModalTitle("");
     };
 
-    const cklickBlockHandler = (e: React.MouseEvent<HTMLElement>, thisDoctor: IDoctorWithUser, text: string) => {
+    const cklickBlockHandler = (
+        e: React.MouseEvent<HTMLElement>,
+        thisDoctor: IDoctorWithUser,
+        text: string
+    ) => {
         e.stopPropagation();
-        setDoctor({...thisDoctor});
+        setDoctor({ ...thisDoctor });
         setShowModal(true);
         setModalTitle(text);
     };
 
-    const cklickActivateHandler = (e: React.MouseEvent<HTMLElement>, thisDoctor: IDoctorWithUser, text: string) => {
+    const cklickActivateHandler = (
+        e: React.MouseEvent<HTMLElement>,
+        thisDoctor: IDoctorWithUser,
+        text: string
+    ) => {
         e.stopPropagation();
-        setDoctor({...thisDoctor});
+        setDoctor({ ...thisDoctor });
         setShowModal(true);
         setModalTitle(text);
     };
 
     const blockUser = async () => {
-        stateDoctor && await blockThisUser(stateDoctor);
+        stateDoctor && (await blockThisUser(stateDoctor));
         stateDoctor && console.log(`Пользователь ${stateDoctor.userId} заблокирован/разблокирован`);
         setShowModal(false);
         clearModalStates();
     };
 
     const activateDoctor = async () => {
-        stateDoctor && await activateThisDoctor(stateDoctor);
+        stateDoctor && (await activateThisDoctor(stateDoctor));
         stateDoctor && console.log(`Врач ${stateDoctor.id} активирован/дезактивирован`);
         setShowModal(false);
         clearModalStates();
@@ -74,7 +93,7 @@ const AllDoctors: React.FunctionComponent = (): React.ReactElement => {
         setShowModal(false);
         clearModalStates();
     };
-    
+
     const navigateCabinetHandler = (e: React.MouseEvent<HTMLElement>, id: string) => {
         e.stopPropagation();
         navigate(`/doctor-cabinet/${id}`);
@@ -85,25 +104,33 @@ const AllDoctors: React.FunctionComponent = (): React.ReactElement => {
             <div className={styles.group_row}>
                 <h1 className={styles.h1_title}>Список врачей</h1>
                 <div className={styles.add_link_colappsed}>
-                    <NavLink className={`${styles.add_icon} ${styles.add_doctor_icon}`}
-                        to={"/admin-page/register-doctor"}></NavLink>
+                    <NavLink
+                        className={`${styles.add_icon} ${styles.add_doctor_icon}`}
+                        to={"/admin-page/register-doctor"}
+                    ></NavLink>
                 </div>
                 <div className={styles.collapsing_link}>
-                    <TransparentLink title={"Зарегистрировать врача"} 
-                        size={EBtnSize.tiny} 
-                        pathTo={"/admin-page/register-doctor"}/>
-                </div>                
-            </div>            
-            <Modal
-                show={showModal}
-                close={modalCancelHandler}>
+                    <TransparentLink
+                        title={"Зарегистрировать врача"}
+                        size={EBtnSize.tiny}
+                        pathTo={"/admin-page/register-doctor"}
+                    />
+                </div>
+            </div>
+            <Modal show={showModal} close={modalCancelHandler}>
                 <div className={styles.modal_flex_column}>
                     <div className={styles.title_box}>
                         <p className={styles.modal_title}>
-                            Вы уверены, что хотите <span className={styles.violet}>{modalTitle}</span> пользователя по имени 
-                            <span className={styles.violet}>{stateDoctor && ` ${stateDoctor.users.surname} ${stateDoctor.users.name}`}</span>?
+                            Вы уверены, что хотите{" "}
+                            <span className={styles.violet}>{modalTitle}</span> пользователя по
+                            имени
+                            <span className={styles.violet}>
+                                {stateDoctor &&
+                                    ` ${stateDoctor.users.surname} ${stateDoctor.users.name}`}
+                            </span>
+                            ?
                         </p>
-                    </div>                    
+                    </div>
                     <div className={styles.modal_btn_group}>
                         <Btn
                             size={EBtnSize.tiny}
@@ -111,94 +138,140 @@ const AllDoctors: React.FunctionComponent = (): React.ReactElement => {
                             btnClass={EBtnClass.white_active}
                             onclick={modalCancelHandler}
                         />
-                        {modalTitle === "разблокировать" || modalTitle === "заблокировать" ?
+                        {modalTitle === "разблокировать" || modalTitle === "заблокировать" ? (
                             <Btn
                                 size={EBtnSize.tiny}
                                 title={"Да"}
                                 btnClass={EBtnClass.dark_active}
                                 onclick={() => blockUser()}
                             />
-                            :
+                        ) : (
                             <Btn
                                 size={EBtnSize.tiny}
                                 title={"Да"}
                                 btnClass={EBtnClass.dark_active}
                                 onclick={() => activateDoctor()}
                             />
-                        }
-                        
+                        )}
                     </div>
                 </div>
             </Modal>
-            {isLoading && <Loader/>}
-            {
-                doctors === undefined || !doctors.length ?
-                    <p>No data</p>
-                    :
-                    <div className={styles.list_table_box}>
-                        <div className={styles.Table_box}>
-                            <table className={styles.Table}>
-                                <thead>
-                                    <tr className={styles.Table_tr}>
-                                        <th className={styles.Table_td_right}>ФИО</th>
-                                        <th className={styles.Table_td_right}>Email</th>
-                                        <th className={styles.Table_td_right}>Tел.</th>
-                                        <th className={styles.Table_td_right}>Специализация</th>
-                                        <th className={styles.Table_td_right}>Блок</th>
-                                        <th className={styles.Table_td}>Активация</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    {doctors.map(({...doctor}) => {
-                                        return (<tr className={styles.Table_tr} key={doctor.id} onClick={(e) => navigateCabinetHandler(e, doctor.userId)}>
-                                            <td className={styles.Table_td_right}>{doctor.users.surname} {doctor.users.name} {doctor.users.patronim ? doctor.users.patronim : ""}</td>
-                                            <td className={styles.Table_td_right}>{doctor.users.email}</td>
-                                            <td className={styles.Table_td_right}>{doctor.users.phone}</td>
-                                            <td className={styles.Table_td_right}>{doctor.speciality}</td>
+            {isLoading && <Loader />}
+            {doctors === undefined || !doctors.length ? (
+                <p>Нет данных</p>
+            ) : (
+                <div className={styles.list_table_box}>
+                    <div className={styles.Table_box}>
+                        <table className={styles.Table}>
+                            <thead>
+                                <tr className={styles.Table_tr}>
+                                    <th className={styles.Table_td_right}>ФИО</th>
+                                    <th className={styles.Table_td_right}>Email</th>
+                                    <th className={styles.Table_td_right}>Tел.</th>
+                                    <th className={styles.Table_td_right}>Специализация</th>
+                                    <th className={styles.Table_td_right}>Блок</th>
+                                    <th className={styles.Table_td}>Активация</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {doctors.map(({ ...doctor }) => {
+                                    return (
+                                        <tr
+                                            className={styles.Table_tr}
+                                            key={doctor.id}
+                                            onClick={(e) =>
+                                                navigateCabinetHandler(e, doctor.userId)
+                                            }
+                                        >
                                             <td className={styles.Table_td_right}>
-                                                {doctor.users.isBlocked ?
-                                                    <div className={styles.switch_td} key={`bl-${doctor.id}`} onClick={(e) => cklickBlockHandler(
-                                                        e, doctor, "разблокировать"
-                                                    )}>
+                                                {doctor.users.surname} {doctor.users.name}{" "}
+                                                {doctor.users.patronim ? doctor.users.patronim : ""}
+                                            </td>
+                                            <td className={styles.Table_td_right}>
+                                                {doctor.users.email}
+                                            </td>
+                                            <td className={styles.Table_td_right}>
+                                                {doctor.users.phone}
+                                            </td>
+                                            <td className={styles.Table_td_right}>
+                                                {doctor.speciality}
+                                            </td>
+                                            <td className={styles.Table_td_right}>
+                                                {doctor.users.isBlocked ? (
+                                                    <div
+                                                        className={styles.switch_td}
+                                                        key={`bl-${doctor.id}`}
+                                                        onClick={(e) =>
+                                                            cklickBlockHandler(
+                                                                e,
+                                                                doctor,
+                                                                "разблокировать"
+                                                            )
+                                                        }
+                                                    >
                                                         <SwitchDiv isOn={true} />
                                                     </div>
-                                                    :
-                                                    <div className={styles.switch_td} key={`bl-${doctor.id}`} onClick={(e) => cklickBlockHandler(
-                                                        e, doctor, "заблокировать"
-                                                    )}>
-                                                        <SwitchDiv isOn={false}/>
+                                                ) : (
+                                                    <div
+                                                        className={styles.switch_td}
+                                                        key={`bl-${doctor.id}`}
+                                                        onClick={(e) =>
+                                                            cklickBlockHandler(
+                                                                e,
+                                                                doctor,
+                                                                "заблокировать"
+                                                            )
+                                                        }
+                                                    >
+                                                        <SwitchDiv isOn={false} />
                                                     </div>
-                                                }
+                                                )}
                                             </td>
                                             <td className={styles.Table_td}>
-                                                {doctor.isActive ?
-                                                    <div className={styles.switch_td} key={`ak-${doctor.id}`} onClick={(e) => cklickActivateHandler(
-                                                        e, doctor, "дезактивировать"
-                                                    )}>
+                                                {doctor.isActive ? (
+                                                    <div
+                                                        className={styles.switch_td}
+                                                        key={`ak-${doctor.id}`}
+                                                        onClick={(e) =>
+                                                            cklickActivateHandler(
+                                                                e,
+                                                                doctor,
+                                                                "дезактивировать"
+                                                            )
+                                                        }
+                                                    >
                                                         <SwitchDiv isOn={true} />
                                                     </div>
-                                                    :
-                                                    <div className={styles.switch_td} key={`ak-${doctor.id}`} onClick={(e) => cklickActivateHandler(
-                                                        e, doctor, "активировать"
-                                                    )}>
-                                                        <SwitchDiv isOn={false}/>
+                                                ) : (
+                                                    <div
+                                                        className={styles.switch_td}
+                                                        key={`ak-${doctor.id}`}
+                                                        onClick={(e) =>
+                                                            cklickActivateHandler(
+                                                                e,
+                                                                doctor,
+                                                                "активировать"
+                                                            )
+                                                        }
+                                                    >
+                                                        <SwitchDiv isOn={false} />
                                                     </div>
-                                                }
+                                                )}
                                             </td>
-                                        </tr>);
-                                    })}
-                                </tbody>
-                            </table>
-                        </div>
-                        {/* <Pagination
+                                        </tr>
+                                    );
+                                })}
+                            </tbody>
+                        </table>
+                    </div>
+                    {/* <Pagination
                             currentPage={currentPage}
                             lastPage={doctors && doctors.length - 1 || 1}
                             maxLength={7}
                             setCurrentPage={setCurrentPage}
                         /> */}
-                    </div> 
-            }
-            
+                </div>
+            )}
         </div>
     );
 };
--- src/containers/AdminPage/AdminTables/AllDoctors/IAllDoctorsProps.ts
+++
@@ -1,5 +0,0 @@
-import IDoctorWithUser from "../../../../interfaces/IDoctor/IDoctorWithUser";
-
-export default interface IAllDoctorsProps {
-    doctors: IDoctorWithUser[]
-}
