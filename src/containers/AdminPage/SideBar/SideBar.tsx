87b133eac91267084e06f642c7c9df04fb599d00
izida kubekova <isedak81@gmail.com>
2023-06-06 07:08:37 +0600

    [DP-91] Создан SideBarLinksArray и применён
     в SideBar

--- src/containers/AdminPage/SideBar/SideBar.tsx
+++ src/containers/AdminPage/SideBar/SideBar.tsx
@@ -1,78 +1,74 @@
-import React, { useState } from "react";
+import { FunctionComponent, MouseEvent, ReactElement, useState } from "react";
 import { NavLink } from "react-router-dom";
 import styles from "./SideBar.module.css";
 import "./SideBarNavLink.css";
 import AccessControl from "../../../permissionRoutes/AccessControl";
 import { ERoles } from "../../../enums/ERoles";
+import { SideBarLinksArray } from "./SideBarLinksArray";
 
-const SideBar: React.FunctionComponent = (): React.ReactElement => {
+const SideBar: FunctionComponent = (): ReactElement => {
     const [isShowingMenu, setIsShowingMenu] = useState(false);
 
-    const toogleMenu = (e: React.MouseEvent<HTMLDivElement>) => {
+    const toogleMenu = (e: MouseEvent<HTMLDivElement>) => {
         e.stopPropagation();
         setIsShowingMenu(!isShowingMenu);
     };
 
     return (
         <>
-            {isShowingMenu ? 
+            {isShowingMenu ? (
                 <div className={styles.sidebar_colappsed_icons_column}>
                     <div className={styles.sidebar_colappsed}>
-                        <div onClick={toogleMenu} 
-                            className={styles.open_btn}>
-                        </div>
+                        <div onClick={toogleMenu} className={styles.open_btn}></div>
                     </div>
-                    <div className={styles.sidebar_colappsed}>
-                        <NavLink className={"icon doctors_icon"}
-                            to={"/admin-page/doctors"}></NavLink>
-                    </div>
-                    <div className={styles.sidebar_colappsed}>
-                        <NavLink className={"icon parents_icon"}
-                            to={"/admin-page/"}></NavLink>
-                    </div>
+                    {SideBarLinksArray.map((link, i) => {
+                        return (
+                            <div key={`l-${i}`} className={styles.sidebar_colappsed}>
+                                <NavLink
+                                    className={`icon ${link.iconClass}`}
+                                    to={link.path}
+                                ></NavLink>
+                            </div>
+                        );
+                    })}
                     <AccessControl allowedRoles={[ERoles.SUPERADMIN]}>
                         <div className={styles.sidebar_colappsed}>
-                            <NavLink className={"icon admins_icon"}
-                                to={"/admin-page/admins"}></NavLink>
+                            <NavLink
+                                className={"icon admins_icon"}
+                                to={"/admin-page/admins"}
+                            ></NavLink>
                         </div>
                         <div className={styles.sidebar_colappsed}>
-                            <NavLink className={"icon statistics_icon"}
-                                to={"/admin-page/"}></NavLink>
+                            <NavLink
+                                className={"icon statistics_icon"}
+                                to={"/admin-page/"}
+                            ></NavLink>
                         </div>
                     </AccessControl>
-                    <div className={styles.sidebar_colappsed}>
-                        <NavLink className={"icon reviews_icon"}
-                            to={"/admin-page/"}></NavLink>
-                    </div>
-                    <div className={styles.sidebar_colappsed}>
-                        <NavLink className={"icon admin_profile_icon"}
-                            to={"/admin-page/profile"}></NavLink>
-                    </div>
                 </div>
-                :
+            ) : (
                 <div className={styles.sidebar}>
                     <div className={styles.header_menu_row}>
                         <h3 className={styles.menu_text}>Меню</h3>
-                        <div onClick={toogleMenu} 
-                            className={styles.close_btn}>
-                        </div>
+                        <div onClick={toogleMenu} className={styles.close_btn}></div>
                     </div>
-                    <NavLink className={"sidebar_link"}
-                        to={"/admin-page/doctors"}>Врачи</NavLink>
-                    <NavLink className={"sidebar_link_"}
-                        to={"/admin-page/"}>Родители пациентов</NavLink>
+                    {SideBarLinksArray.map((fullLink, i) => {
+                        return (
+                            <NavLink key={`fl-${i}`} className={"sidebar_link"} to={fullLink.path}>
+                                {fullLink.label}
+                            </NavLink>
+                        );
+                    })}
                     <AccessControl allowedRoles={[ERoles.SUPERADMIN]}>
-                        <NavLink className={"sidebar_link_"}
-                            to={"/admin-page/admins"}>Администраторы</NavLink>
-                        <NavLink className={"sidebar_link_"}
-                            to={"/admin-page/"}>Статистика</NavLink>
-                    </AccessControl>                    
-                    <NavLink className={"sidebar_link_"}
-                        to={"/admin-page/"}>Отзывы</NavLink>
-                    <NavLink className={"sidebar_link_"}
-                        to={"/admin-page/profile"}>Мой профиль</NavLink>
+                        <NavLink className={"sidebar_link_"} to={"/admin-page/admins"}>
+                            Администраторы
+                        </NavLink>
+                        <NavLink className={"sidebar_link_"} to={"/admin-page/"}>
+                            Статистика
+                        </NavLink>
+                    </AccessControl>
                 </div>
-            }
+            )}
         </>
     );
 };
