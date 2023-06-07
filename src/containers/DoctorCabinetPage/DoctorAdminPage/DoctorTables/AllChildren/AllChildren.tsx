5948e35e93dcecb4cb729494ac1a034df8ac36ff
izida kubekova <isedak81@gmail.com>
2023-06-07 19:50:26 +0600

    [DP-93] AllChildren.tsx разбит на компоненты

--- src/containers/DoctorCabinetPage/DoctorAdminPage/DoctorTables/AllChildren/AllChildren.tsx
+++ src/containers/DoctorCabinetPage/DoctorAdminPage/DoctorTables/AllChildren/AllChildren.tsx
@@ -1,7 +1,5 @@
-import React from "react";
-// import Pagination from "../../../../components/UI/Pagination/Pagination";
+import { FunctionComponent, ReactElement, useEffect, useState } from "react";
 import styles from "../../../../AdminPage/AdminTables/AllTables.module.css";
-// import { NavLink, useNavigate } from "react-router-dom";
 import { useGetChildrenByDoctorQuery } from "../../../../../app/services/children";
 import { toast } from "react-toastify";
 import { FetchBaseQueryError } from "@reduxjs/toolkit/dist/query";
@@ -9,32 +7,41 @@ import { SerializedError } from "@reduxjs/toolkit";
 import { IErrorResponse } from "../../../../../interfaces/IUser/IErrorResponse";
 import { IMessage } from "../../../../../interfaces/IUser/IMessage";
 import Loader from "../../../../../components/UI/Loader/Loader";
-import defaultChildPhoto from "../../../../../assets/img/default-child-photo.svg";
 import { useDoctorId } from "../../DoctorAdminPage";
+import Pagination from "../../../../../components/UI/Pagination/Pagination";
+import AllChildrenTable from "./AllChildrenTable/AllChildrenTable";
 
-const AllChildren: React.FunctionComponent = (): React.ReactElement => {
+const AllChildren: FunctionComponent = (): ReactElement => {
     const { doctorId } = useDoctorId();
-    // const navigate = useNavigate();
-    // const [currentPage, setCurrentPage] = useState(1);
+    const limit = 10;
+    const [pages, setPages] = useState<number>(0);
+    const [offset, setOffset] = useState<number>(0);
+    const [currentPage, setCurrentPage] = useState(1);
     const {
         data: children,
         error: getChildrenError,
         isError: isChildrenGetError,
-        isLoading,
-    } = useGetChildrenByDoctorQuery({ offset: 0, limit: 10, id: doctorId });
+        isLoading
+    } = useGetChildrenByDoctorQuery({ offset, limit, id: doctorId });
+
+    useEffect(() => {
+        if (children && children.rows) {
+            children.count % limit !== 0
+                ? setPages(Math.floor(children.count / limit) + 1)
+                : setPages(children.count / limit);
+        }
+    }, []);
+
+    useEffect(() => {
+        setOffset((currentPage - 1) * limit);
+    }, [currentPage]);
 
     const errorHandler = (data: FetchBaseQueryError | SerializedError | undefined) => {
         const err = data as IErrorResponse<IMessage>;
-        toast.error(`Ошибка ${err.data.message} Статус: ${err.status}`);
+        toast.error(`Ошибка ${err.data.message}`);
     };
 
     isChildrenGetError && errorHandler(getChildrenError);
-
-    const navigateCabinetHandler = (e: React.MouseEvent<HTMLElement>, id: string) => {
-        e.stopPropagation();
-        console.log(`Переход на страницу ребёнка с id: ${id}`);
-        // navigate(`/child-cabinet/${id}`);
-    };
 
     return (
         <div className={styles.list_container}>
@@ -42,76 +49,21 @@ const AllChildren: React.FunctionComponent = (): React.ReactElement => {
                 <h1 className={styles.h1_title}>Список пациентов</h1>
             </div>
             {isLoading && <Loader />}
-            {children === undefined || !children.length ? (
+            {children === undefined || !children.rows.length ? 
                 <p>Нет данных</p>
-            ) : (
+                : 
                 <div className={styles.list_table_box}>
-                    <div className={styles.Table_box}>
-                        <table className={styles.Table}>
-                            <thead>
-                                <tr className={styles.Table_tr}>
-                                    <th className={styles.Table_td_right}>Фото</th>
-                                    <th className={styles.Table_td_right}>ФИО</th>
-                                    <th className={styles.Table_td_right}>Дата рождения</th>
-                                    <th className={styles.Table_td_right}>Пол</th>
-                                    <th className={styles.Table_td}>Вес/Рост</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                {children.map(({ ...child }) => {
-                                    return (
-                                        <tr
-                                            className={styles.Table_tr}
-                                            key={child.id}
-                                            onClick={(e) => navigateCabinetHandler(e, child.id)}
-                                        >
-                                            <td
-                                                className={`${styles.Table_td_right} ${styles.td_for_image}`}
-                                            >
-                                                {child.photo !== "" ? (
-                                                    <img
-                                                        className={styles.childPhoto}
-                                                        onError={(e) => {
-                                                            e.currentTarget.src = defaultChildPhoto;
-                                                        }}
-                                                        src={`${
-                                                            import.meta.env.VITE_BASE_URL
-                                                        }uploads/childrenImgs/${child.photo}`}
-                                                        alt={"фото"}
-                                                    />
-                                                ) : (
-                                                    <img
-                                                        className={styles.childPhoto}
-                                                        src={defaultChildPhoto}
-                                                        alt={"фото"}
-                                                    />
-                                                )}
-                                            </td>
-                                            <td className={styles.Table_td_right}>
-                                                {child.surname} {child.name}{" "}
-                                                {child.patronim ? child.patronim : ""}
-                                            </td>
-                                            <td className={styles.Table_td_right}>
-                                                {new Date(child.dateOfBirth).toLocaleDateString()}
-                                            </td>
-                                            <td className={styles.Table_td_right}>{child.sex}</td>
-                                            <td className={styles.Table_td}>
-                                                {child.weight}кг/{child.height}см
-                                            </td>
-                                        </tr>
-                                    );
-                                })}
-                            </tbody>
-                        </table>
-                    </div>
-                    {/* <Pagination
+                    <AllChildrenTable allChildren={children.rows} />
+                    {pages > 1 ? (
+                        <Pagination
                             currentPage={currentPage}
-                            lastPage={doctors && doctors.length - 1 || 1}
+                            lastPage={pages}
                             maxLength={7}
                             setCurrentPage={setCurrentPage}
-                        /> */}
+                        />
+                    ) : null}
                 </div>
-            )}
+            }
         </div>
     );
 };
---
+++ src/containers/DoctorCabinetPage/DoctorAdminPage/DoctorTables/AllChildren/AllChildrenTable/AllChildrenTable.tsx
@@ -0,0 +1,72 @@
+import { FunctionComponent, ReactElement } from "react";
+import IAllChildrenTableProps from "./IAllChildrenTableProps";
+import styles from "../../../../../AdminPage/AdminTables/AllTables.module.css";
+import { useNavigate } from "react-router-dom";
+import defaultChildPhoto from "../../../../../../assets/img/default-child-photo.svg";
+
+const AllChildrenTable: FunctionComponent<IAllChildrenTableProps> = (props: IAllChildrenTableProps): ReactElement => {
+    const navigate = useNavigate();
+
+    const navigateCabinetHandler = (e: React.MouseEvent<HTMLElement>, id: string) => {
+        e.stopPropagation();
+        navigate(`/child-cabinet/${id}`);
+    };
+
+    return (
+        <div className={styles.Table_box}>
+            <table className={styles.Table}>
+                <thead>
+                    <tr className={styles.Table_tr}>
+                        <th className={styles.Table_td_right}>Фото</th>
+                        <th className={styles.Table_td_right}>ФИО</th>
+                        <th className={styles.Table_td_right}>Дата рождения</th>
+                        <th className={styles.Table_td_right}>Пол</th>
+                        <th className={styles.Table_td}>Вес/Рост</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {props.allChildren.map(({ ...child }) => {
+                        return (
+                            <tr className={styles.Table_tr} key={child.id}
+                                onClick={(e) => navigateCabinetHandler(e, child.id)}>
+                                <td className={`${styles.Table_td_right} ${styles.td_for_image}`}>
+                                    {child.photo !== "" ? (
+                                        <img
+                                            className={styles.childPhoto}
+                                            onError={(e) => {
+                                                e.currentTarget.src = defaultChildPhoto;
+                                            }}
+                                            src={`${
+                                                import.meta.env.VITE_BASE_URL
+                                            }uploads/childrenImgs/${child.photo}`}
+                                            alt={"фото"}
+                                        />
+                                    ) : (
+                                        <img
+                                            className={styles.childPhoto}
+                                            src={defaultChildPhoto}
+                                            alt={"фото"}
+                                        />
+                                    )}
+                                </td>
+                                <td className={styles.Table_td_right}>
+                                    {child.surname} {child.name}{" "}
+                                    {child.patronim ? child.patronim : ""}
+                                </td>
+                                <td className={styles.Table_td_right}>
+                                    {new Date(child.dateOfBirth).toLocaleDateString()}
+                                </td>
+                                <td className={styles.Table_td_right}>{child.sex}</td>
+                                <td className={styles.Table_td}>
+                                    {child.weight}кг/{child.height}см
+                                </td>
+                            </tr>
+                        );
+                    })}
+                </tbody>
+            </table>
+        </div>
+    );
+};
+
+export default AllChildrenTable;
