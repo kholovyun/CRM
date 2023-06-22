5531cdf0d0c94cbc4919bed9699f3d9b2fee0704
ruslan <bumerboy86@gmail.com>
2023-06-22 10:07:59 +0600

    созданы компоненты для ссылок

---
+++ src/components/UI/LinkWithChildren/LinkWithChildren.tsx
@@ -0,0 +1,27 @@
+import styles from "./LinkWithChildren.module.css";
+import {useState} from "react";
+
+type TLinkWithClidren = {
+    text: string,
+    fn: () => void,
+};
+const LinkWithChildren = (props:TLinkWithClidren) => {
+    const [showChild, SetShowChild] = useState(false);
+    const showChildren = ():void => {
+        SetShowChild(!showChild);
+        !showChild && props.fn();
+    };
+
+    return (
+        <>
+            <div className={styles.linkWithChildren} onClick={showChildren}>
+                <p className={styles.linkWithChildrenText}>{props.text}</p>
+                <div className={styles.linkWithChildrenArrow}></div>
+            </div>
+            {showChild && <div>Hello Man</div>}
+        </>
+
+    );
+};
+
+export default LinkWithChildren;
