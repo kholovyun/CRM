94dbf60b22bb42c3205808b2a474f57a2461ecf9
ruslan <bumerboy86@gmail.com>
2023-06-27 15:24:29 +0600

    создан компонент ActivationForm

---
+++ src/containers/UserForms/ActivationForm/ActivationForm.tsx
@@ -0,0 +1,10 @@
+import styles from "ActivationForm.module.css";
+const ActivationForm = () => {
+    return (
+        <div className={styles.activationForm}>
+            <p>Активация вашего аккаунта произойдет после того, как вы ознакомитесь с договором оферты, политикой конфиденциальности и сведениями, внесенными в личный кабинет</p>
+        </div>
+    );
+};
+
+export default ActivationForm;
