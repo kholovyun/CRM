2048cf1c044a42ecce54f2387579796896196d25
albina_rakhmetova <rakhmetovaazh@gmail.com>
2023-06-29 10:01:09 +0500

    [DP-166] Место приема выведено в таблицах приемов

--- src/components/ChildVisits/VisitRow/VisitRow.tsx
+++ src/components/ChildVisits/VisitRow/VisitRow.tsx
@@ -6,7 +6,7 @@ const VisitsRow: FunctionComponent<IVisitRowProps> = (props): ReactElement => {
     return (
         <tr className={styles.visitsTable_tr}>
             <td className={styles.visitsTable_td_right}>{new Date(props.visit.date).toLocaleDateString()}</td>
-            <td className={styles.visitsTable_td_right}>{}</td>
+            <td className={styles.visitsTable_td_right}>{props.visit.place}</td>
             <td className={styles.visitsTable_td_right}>{props.visit.reason}</td>
             <td className={styles.visitsTable_td_right}>{props.visit.clinicData}</td>
             <td className={styles.visitsTable_td_right}>{props.visit.conclusion}</td>
