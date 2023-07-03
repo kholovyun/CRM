b5e41b48ba9580f6d63f48d676ea647560d636e8
albina_rakhmetova <rakhmetovaazh@gmail.com>
2023-07-03 10:08:19 +0500

    [DP-175] Исправлена стилизация таблиц в блоке приемы у врача

--- src/components/ChildVisits/ChildVisits.tsx
+++ src/components/ChildVisits/ChildVisits.tsx
@@ -8,8 +8,11 @@ import { EBtnSize } from "../../enums/EBtnSize";
 import { EBtnTypes } from "../../enums/EBtnTypes";
 import { EBtnClass } from "../../enums/EBtnClass";
 import Modal from "../UI/Modal/Modal";
+import { ERoles } from "../../enums/ERoles";
+import { useAppSelector } from "../../app/hooks";
 
 const ChildVisits: FunctionComponent<IChildVisitsProps> = (props): ReactElement => {
+    const { user } = useAppSelector(state => state.auth);
     const [showAddModal, setShowAddModal] = useState(false);
 
     const addNewVisitCloser = () => {
@@ -25,12 +28,13 @@ const ChildVisits: FunctionComponent<IChildVisitsProps> = (props): ReactElement 
                 <table className={stylesTable.Table}>
                     <thead>
                         <tr className={stylesTable.Table_tr}>
-                            <th className={styles.visitsTable_td_right}>Дата осмотра</th>
-                            <th className={styles.visitsTable_td_right}>Место осмотра</th>
-                            <th className={styles.visitsTable_td_right}>Характер посещение</th>
-                            <th className={styles.visitsTable_td_right}>Клинические данные</th>
-                            <th className={styles.visitsTable_td_right}>Заключение</th>
-                            <th className={styles.visitsTable_td}>Назначение</th>
+                            <th className={stylesTable.Table_td_right}>Дата осмотра</th>
+                            <th className={stylesTable.Table_td_right}>Место осмотра</th>
+                            <th className={stylesTable.Table_td_right}>Характер посещение</th>
+                            <th className={stylesTable.Table_td_right}>Клинические данные</th>
+                            <th className={stylesTable.Table_td_right}>Заключение</th>
+                            <th className={user?.role === ERoles.DOCTOR ? stylesTable.Table_td_right: stylesTable.Table_td}>Назначение</th>
+                            {user?.role === ERoles.DOCTOR ? <th className={stylesTable.Table_td}></th> : null}
                         </tr>
                     </thead>
                     <tbody>
--- src/components/ChildVisits/VisitRow/IVisitRowProps.ts
+++ src/components/ChildVisits/VisitRow/IVisitRowProps.ts
@@ -2,4 +2,5 @@ import IVisitGetDto from "../../../interfaces/IVisit/IVisitGetDto";
 
 export default interface IVisitRowProps {
     visit: IVisitGetDto
+    deleteVisit: () => void
 }
