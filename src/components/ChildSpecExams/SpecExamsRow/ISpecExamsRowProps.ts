c8c56fc1a37b5aad380f07eae82b6fa3f35a1f79
albina_rakhmetova <rakhmetovaazh@gmail.com>
2023-07-10 12:06:34 +0500

    [DP-190] Создан компонент-таблица да осмотров

---
+++ src/components/ChildSpecExams/SpecExamsRow/ISpecExamsRowProps.ts
@@ -0,0 +1,5 @@
+import ISpecialistExamsGetDto from "../../../interfaces/ISpecialistExams/ISpecialistExamsGetDto";
+
+export default interface ISpecExamsRowProps {
+    specExam: ISpecialistExamsGetDto
+}
