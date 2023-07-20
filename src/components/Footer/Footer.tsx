d03a6594e4dc9f4fabf25da1975584843baac1f9
ruslan <bumerboy86@gmail.com>
2023-07-20 20:52:50 +0600

    test

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -3,6 +3,14 @@ import styles from "./Footer.module.css";
 import { NavLink } from "react-router-dom";
 
 const Footer: FunctionComponent = (): ReactElement => {
+    function openGmailCompose():void {
+        const email = "zabotik.help@yandex.ru";
+        const subject:string = encodeURIComponent("Тема письма");
+        const body:string = encodeURIComponent("Текст письма");
+
+        const gmailComposeUrl:string = "https://mail.google.com/mail/?view=cm&fs=1&to=" + email + "&su=" + subject + "&body=" + body;
+        window.open(gmailComposeUrl);
+    }
     return (
         <div className={styles.Footer_bg}>
             <div className={styles.Footer_content_container}>
@@ -14,8 +22,18 @@ const Footer: FunctionComponent = (): ReactElement => {
                     <p className={styles.FooterContactsTitle}>Контакты для связи</p>
                     <div className={styles.icoContactBox}>
                         <div className={`${styles.icoContact}`}></div>
-                        <div className={`${styles.icoContact} ${styles.icoContactItemTelegram}`}></div>
-                        <div className={`${styles.icoContact} ${styles.icoContactItemWhatsapp}`}></div>
+                        <a 
+                            href="https://t.me/Alinaardamina" 
+                            className={`${styles.icoContact} ${styles.icoContactItemTelegram}`}
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        />
+                        <a 
+                            href="#" 
+                            onClick={openGmailCompose}
+                            target={"_blank"}
+                            className={`${styles.icoContact} ${styles.icoContactItemWhatsapp}`}
+                        />
                     </div>
                 </div>
             </div>
